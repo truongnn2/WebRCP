cc6c8059b567f6db516c9fc37f29ab94d0466dbd
tronghieu_IT\tronghieu <tronghieuhoho@gmail.com>
2018-08-23 21:01:41 +0700

    Update solution

---
+++ RCP/Areas/Admin/Controllers/AdminManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RCP.Areas.Admin.Controllers
+{
+    public class AdminManagerController : Controller
+    {
+        // GET: Admin/AdminManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
